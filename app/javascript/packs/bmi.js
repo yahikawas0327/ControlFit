64ab26f5702abd3b8549a14bfe96d8dd6f632807
張維傑 <jie255057@gmail.com>
2019-12-18 14:04:31 +0800

    abc

---
+++ app/javascript/packs/bmi.js
@@ -0,0 +1,23 @@
+console.log('123')
+
+debugger
+
+$(document).ready(function(){
+  $('.submit').click(function(){
+    var h = $(".cm").val();
+    var w = $(".kg").val();
+    if(isNaN(h)){
+      alert("請輸入數字");
+      height.value="";
+    }else if(isNaN(w)){
+      alert("請輸入數字");
+      weight.value="";
+    }else{
+      h = parseFloat(h) / 100;
+      w = parseFloat(w)
+      var bmi = w / (h * h);
+      bmi = bmi.toFixed(2);
+      $(".box").append("身高" + h + "公尺！" + "體重" + w + "公斤!" + "\n BMI = " + bmi);
+    }
+  })
+})
