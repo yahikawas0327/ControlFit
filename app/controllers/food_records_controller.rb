952a88a8ce26f320a9c98c57d93ca1ca4e7ec285
HenryYHHsu <henryhsu0529@gmail.com>
2019-12-21 20:40:45 +0800

    Finish Daliy food record version-1.0.0

--- app/controllers/food_records_controller.rb
+++ app/controllers/food_records_controller.rb
@@ -2,14 +2,23 @@ class FoodRecordsController < ApplicationController
    
     def create
         # Obtain Post data from Query system
-        food_record= FoodDatabase.find_by(id:params[:food_id])
-        food_qty=params[:food_qty]
-        food_type=params[:food_type]
-        total_calories= (food_qty.to_i) * (food_record.calories.round(2))
-       
+        if params[:food_id] == 0
+           
+           food_name     = params[:food_name]
+           food_calories = (params[:food_calories]).to_i
+        else 
+           food_record   = FoodDatabase.find_by(id:params[:food_id])
+           food_name     = food_record.name
+           food_calories = food_record.calories.round(2)
+        end
+
+           food_qty=params[:food_qty]
+           food_type=params[:food_type]
+           total_calories= (food_qty.to_i) * (food_calories.round(2))
+                  
         # Write Daily food data to FoodRecord database
-        FoodRecord.create(:name => food_record.name , 
-                          :calories => food_record.calories.round(2),
+        FoodRecord.create(:name => food_name , 
+                          :calories => food_calories,
                           :qty => food_qty,
                           :eat_type => food_type,
                           :total_calorie => total_calories  )
--- app/javascript/scripts/index.js
+++ app/javascript/scripts/index.js
@@ -8,33 +8,16 @@ import './application.js'
 import $ from 'jquery'
 import axios from 'axios-on-rails'
 // const axios = require('axios');
-
-
-// 新增每日食物資料 JQuery 
-
-// $(document).ready(function (){
-//     $('.search_food_result').on('click','button',function(evt){
-//         evt.stopPropagation();
-//         let food_hash = {
-//             food_id : `${evt.target.id}`
-//          }
-//          console.log(food_hash)
-//         axios.post("http://localhost:3000/food_records", food_hash)
-//          .then( response => {
-//             console.log('response=>',response); 
-//          })
-//         // console.log(food_hash)
-
-//     })
-// })
-
- // 新增每日食物資料 JQuery 
+ 
  $(document).ready(function (){
-    $('tr').on('click','button',function(evt){
 
+ 
+    // 新增每日食物資料 JQuery  
+    $('tr').on('click','button',function(evt){
       $('.table-hover').hide();
       let food_name = $(evt.target).parent().siblings('td:first').text()
       let food_calorie = $(evt.target).parent().siblings('td:first').next().text()
+      $('#Add_food_record_type').remove()
 
        $('#Add_food_record').append( `
        <div class="col-md-3 mb-3 ">
@@ -71,15 +54,12 @@ import axios from 'axios-on-rails'
     $('#Add_food_record').on('click','button',function(evt){
       evt.stopPropagation();
       let foodqty=$("#foodqty :selected").text();
-      //獲取Select選擇的qty
       let foodtype=$("#foodtype :selected").text();
-      //獲取Select選擇的foodtype
       let food_hash = {
                 food_id : `${evt.target.id}`,
                 food_qty: foodqty,
                 food_type: foodtype
                  }
-      // console.log(food_hash)
       axios.post("http://localhost:3000/food_records", food_hash)
            .then( response => {
                   console.log('response=>',response); 
@@ -88,7 +68,61 @@ import axios from 'axios-on-rails'
  
     //----------------------------------------------------------
 
+    $('#Add_food_record_by_user').on('click', function(){
+     $('.table-hover').remove();
+     $('#Add_food_record').remove()  
+     $('#Add_food_record_type').append(`
+     <div class="col-md-3 mb-3 ">
+       <label for="exampleFormControlInput1">食物名稱</label>
+       <input type="text" class="form-control" id="foodname"  placeholder="食物名稱">
+     </div>
+     <div class="col-md-3 mb-3">
+       <label for="exampleFormControlInput1">卡洛里</label>
+       <input type="text" class="form-control" id="foodcalories"  placeholder="卡洛里">
+     </div>
+     <div class="col-md-3 mb-3">
+     <label for="custom-select">份數</label>
+     <select class="custom-select" id="foodqty">
+         <option selected>1</option>
+         <option value="2">2</option>
+         <option value="3">3</option>
+         <option value="4">4</option>
+         <option value="5">5</option>
+     </select>
+     </div>
+     <div class="col-md-3 mb-3">
+     <label for="exampleFormControlInput1">型態</label>
+     <select class="custom-select" id="foodtype">
+     <option selected>早餐</option>
+     <option value="1">午餐</option>
+     <option value="2">晚餐</option>
+     <option value="3">點心/其他</option>
+     </select>
+     </div>
+     <button type="submit" class="fas fa-cloud-upload-alt" id="Add_food_data_by_user"></button>
+   `)
 
+    }) // Create New food record data      
+    $('#Add_food_record_type').on('click','button', function(){
+
+       let foodname=$('#foodname').val();
+       let foodcalories=$('#foodcalories').val();
+       let foodqty=$("#foodqty :selected").text();
+       let foodtype=$("#foodtype :selected").text();
+       let food_hash = {
+          food_id     : 0,
+          food_name   : foodname,
+          food_calories: foodcalories,
+          food_qty    : foodqty,
+          food_type   : foodtype
+           }
+       axios.post("http://localhost:3000/food_records", food_hash)
+            .then( response => {
+                  console.log('response=>',response); 
+      })
+
+    })
+      
 
 })
 
