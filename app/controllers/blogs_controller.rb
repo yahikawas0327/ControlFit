4ccddd6eb8ea14f3f5bb888b0febc863bb0c7a8f
張維傑 <jie255057@gmail.com>
2019-12-25 18:51:50 +0800

    add blog CRUD

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -16,6 +16,25 @@ class BlogsController < ApplicationController
     end
   end
 
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path
+    else
+      render edit_blog_path
+    end
+  end
+
+  def destroy
+    @blog = Blog.find(params[:id])
+    if @blog.destroy
+      redirect_to blogs_path
+    end
+  end
   private
 
   def blog_params
