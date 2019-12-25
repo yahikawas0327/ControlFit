291238f99bd383e29fc97c51a1917ac630057b7b
張維傑 <jie255057@gmail.com>
2019-12-25 12:48:18 +0800

    db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_12_23_095444) do
-
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +33,7 @@ ActiveRecord::Schema.define(version: 2019_12_23_095444) do
     t.string "eat_type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "total_calorie"
   end
 
   create_table "members", force: :cascade do |t|
