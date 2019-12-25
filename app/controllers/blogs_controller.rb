38f3a137250bb1a53acd753985f7ac827ea7da9e
張維傑 <jie255057@gmail.com>
2019-12-25 17:50:08 +0800

    add blog

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,23 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blog_path, notice:'新增成功'
+    else
+      render new_blog_path, notice:'新增失敗'
+    end
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :area)
+  end
+end
