41fc6fef83e76a6b09002fa6c7bc306f8d40f843
HenryYHHsu <henryhsu0529@gmail.com>
2019-12-29 21:23:22 +0800

    Add Sport-record by vue version-1.0.1

--- app/javascript/packs/sport_record.js
+++ app/javascript/packs/sport_record.js
@@ -57,6 +57,16 @@ document.addEventListener('DOMContentLoaded', () => {
              .then(function(response){
                   console.log(response)
              })
+        this.daily_sport.push({
+          id: this.sports[idx].id ,
+          name: this.sports[idx].name,
+          weight: this.weight,
+          min: Number(this.min).toFixed(1),
+          totalconsum: this.sports[idx].computed,
+          created_at: moment().calendar()
+        })
+        this.daily_count += 1;
+        this.daily_sum += Number(this.sports[idx].computed)
       },
       updateCurrentTime() {
         this.currentTime = moment().format('LTS');
@@ -88,6 +98,7 @@ document.addEventListener('DOMContentLoaded', () => {
                })
            .then(function(response){
                let daily_sport = response.data
+               console.log(daily_sport )
                for (var i in daily_sport ){
                 self.daily_sport.push(daily_sport[i])
                 var NowDate = new Date(daily_sport[i].created_at)
