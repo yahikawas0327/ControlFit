0c5ab7a7f0aa0aedf5af666b672c460c118a6aff
張維傑 <jie255057@gmail.com>
2020-01-02 17:42:20 +0800

    add Relationship validate

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,9 @@ class Relationship < ApplicationRecord
   belongs_to :followed, class_name: "Member"
   validates :follower_id, presence: true
   validates :followed_id, presence: true
+  validate :not_following_himself
+
+  def not_following_himself 
+    errors.add :followed_id if followed_id == follower_id
+  end
 end
