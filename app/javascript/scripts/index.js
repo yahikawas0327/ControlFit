9d69ffcbc100b018bdfe4af6bff548cbfca5b575
HenryYHHsu <henryhsu0529@gmail.com>
2020-01-03 23:20:34 +0800

    Add daily-record crud version--0.0.2

--- app/javascript/scripts/index.js
+++ app/javascript/scripts/index.js
@@ -9,10 +9,11 @@ import './application.js'
 import $ from 'jquery'
 import axios from 'axios-on-rails'
 import moment from "moment";
+import { element } from 'prop-types';
 
 // const axios = require('axios');
  
-$(document).on('turbolinks:load', function(){
+$(document).ready(function(){
 
     // 新增每日食物資料 JQuery  
     $('.search_food_result').on('click','button',function(evt){
@@ -22,46 +23,57 @@ $(document).on('turbolinks:load', function(){
       $('#Add_food_record_type').remove()
 
        $('#Add_food_record').append( `
+       <div class="form-row" >
+       <div class="col-md-1 md-3"></div>
        <div class="col-md-3 mb-3 ">
-       <label for="disabledTextInput">食物名稱</label>
-       <input class="form-control" type="text" placeholder="${food_name}" readonly>
+        <label for="disabledTextInput">食物名稱</label>
+        <input class="form-control" type="text" placeholder="${food_name}" readonly>
        </div>
-       <div class="col-md-3 mb-3">
-       <label for="disabledTextInput_1">卡洛里</label>
-       <input class="form-control" type="text" placeholder="${food_calorie}" readonly>
+       <div class="col-md-2 mb-3">
+        <label for="disabledTextInput_1">卡洛里</label>
+        <input class="form-control" type="text" placeholder="${food_calorie}" readonly>
        </div>
-       <div class="col-md-3 mb-3">
-            <label for="custom-select">份數</label>
-            <select class="custom-select" id="foodqty">
-                <option selected>1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-            </select>
+       <div class="col-md-1 mb-3">
+        <label for="custom-select">份數</label>
+        <select class="custom-select" id="foodqty">
+            <option selected>1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+        </select>
        </div>
-       <div class="col-md-3 mb-3">
-            <label for="exampleFormControlInput1">型態</label>
-            <select class="custom-select" id="foodtype">
-                <option selected>早餐</option>
-                <option value="1">午餐</option>
-                <option value="2">晚餐</option>
-                <option value="3">點心/其他</option>
-            </select>
+       <div class="col-md-2 mb-3">
+        <label for="exampleFormControlInput1">型態</label>
+        <select class="custom-select" id="foodtype">
+            <option selected>早餐</option>
+            <option value="1">午餐</option>
+            <option value="2">晚餐</option>
+            <option value="3">點心/其他</option>
+        </select>
        </div>
-       <button type="submit" class="fas fa-cloud-upload-alt" id="${evt.target.id}"></button>
+       <div class="col-md-1 md-3">
+         <label for="exampleFormControlInput1">加進去</label>
+         <button type="submit" class="button is-success  is-light js-add" data-id="${evt.target.id}"><i class="fas fa-plus-square"></i></button>       
+       </div>
+       <div class="col-md-1 mb-3">
+       <label for="exampleFormControlInput1">回搜尋</label>
+       <button class="button  is-link is-light is-return" data-id="return_food"><i class="fas fa-undo-alt"></i></button>
+       </div>
+       </div>
        `); // 增加使用者體驗 並接收 食物數量 / 型態
     })
-     
-    $('#Add_food_record').on('click','button',function(evt){
+    
+    $('.form-row').on('click','.js-add',function(evt){
       evt.stopPropagation();
       let foodqty=$("#foodqty :selected").text();
       let foodtype=$("#foodtype :selected").text();
       let food_hash = {
-                food_id : `${evt.target.id}`,
+                food_id : `${this.dataset.id}`,
                 food_qty: foodqty,
                 food_type: foodtype
                  }
+       $(this).parent().parent('.form-row').remove()
       axios.post("http://localhost:3000/food_records", food_hash)
            .then( response => {
                   console.log('response=>',response);
@@ -70,21 +82,27 @@ $(document).on('turbolinks:load', function(){
     }) // 將資料傳送至後端 傳送 id / qty / type
  
     //----------------------------------------------------------
+    $('.form-row').on('click','.is-return', function(){
+      $(this).parent().parent('.form-row').remove()
+      $('#query-food').show();
+    })
 
     $('#Add_food_record_by_user').on('click', function(){
-     // $('.table-hover').remove();
      $('#query-food').remove();
      $('#Add_food_record').remove()  
      $('#Add_food_record_type').append(`
+     <div class="form-row" >
+     <div class="col-md-1 mb-3">
+     </div>
      <div class="col-md-3 mb-3 ">
        <label for="exampleFormControlInput1">食物名稱</label>
        <input type="text" class="form-control" id="foodname"  placeholder="食物名稱">
      </div>
-     <div class="col-md-3 mb-3">
+     <div class="col-md-2 mb-3">
        <label for="exampleFormControlInput1">卡洛里</label>
        <input type="text" class="form-control" id="foodcalories"  placeholder="卡洛里">
      </div>
-     <div class="col-md-3 mb-3">
+     <div class="col-md-2 mb-3">
      <label for="custom-select">份數</label>
      <select class="custom-select" id="foodqty">
          <option selected>1</option>
@@ -94,7 +112,7 @@ $(document).on('turbolinks:load', function(){
          <option value="5">5</option>
      </select>
      </div>
-     <div class="col-md-3 mb-3">
+     <div class="col-md-2 mb-3">
      <label for="exampleFormControlInput1">型態</label>
      <select class="custom-select" id="foodtype">
      <option selected>早餐</option>
@@ -103,12 +121,17 @@ $(document).on('turbolinks:load', function(){
      <option value="3">點心/其他</option>
      </select>
      </div>
-     <button type="submit" class="fas fa-cloud-upload-alt" id="Add_food_data_by_user"></button>
+     <div class="col-md-1 mb-3">
+     <label for="exampleFormControlInput1">加進去</label>
+     <button class="button is-success is-small is-light is-add" data-id="Add_food_data_by_user"><i class="fas fa-plus-square"></i></button>
+     </div>
+     </div>
    `)
+    }) // Create New food record data
 
-    }) // Create New food record data      
-    $('#Add_food_record_type').on('click','button', function(){
-
+    $('.form-row').on('click','.is-add', function(){
+        // evt.stopPropagation();
+        console.log(this.dataset.id)
        let foodname=$('#foodname').val();
        let foodcalories=$('#foodcalories').val();
        let foodqty=$("#foodqty :selected").text();
@@ -120,25 +143,109 @@ $(document).on('turbolinks:load', function(){
           food_qty    : foodqty,
           food_type   : foodtype
            }
+           console.log(food_hash)
+           $(this).parent().parent('.form-row').remove()
        axios.post("http://localhost:3000/food_records", food_hash)
             .then( response => {
               console.log('response=>',response);
-              $( "#daliy-food" ).load( "search_food.html #daliy-food" );
-  })
+              $( "#daliy-food" ).load( "search_food.html #daliy-food" )
+              deleteEvent()
+              editEvent()
+            })
 
-    })
+    }) // Add New food data
+        
     // Edit daily record data function 
-    $('.daily_food_result').on('click','.js-edit',function(){
-       alert(this.dataset.id);
-    })
+    function editEvent(){
+      $('.daily_food_result').on('click','.js-edit',function(evt){
+        console.log(this)
+        $(this).attr('disabled', 'disabled')
+        alert(this.dataset.id);
+        evt.stopPropagation();
+        let edit_food_id = this.dataset.id
+        $('#Add_food_record_type').remove()
+        $('#query-food').remove();
+        let edit_food_qty = $(this).parent().siblings('.qty:eq(0)').text()
+        let edit_food_name = $(this).parent().siblings('.name:eq(0)').text()
+        let edit_food_calorie = $(this).parent().siblings('.foodsum:eq(0)').text()
+        let edit_food_type = $(this).parent().siblings('.type:eq(0)').text()
+        $('#Edit_food_record').append( `
+        <div class="form-row" >
+        <div class="col-md-1 md-3 ">
+        </div>
+        <div class="col-md-3 mb-3 ">
+        <label for="disabledTextInput">食物名稱</label>
+        <input class="form-control" type="text" placeholder="${edit_food_name}" readonly>
+        </div>
+        <div class="col-md-2 mb-3">
+        <label for="disabledTextInput_1">卡洛里</label>
+        <input class="form-control" type="text" placeholder="${edit_food_calorie}" readonly>
+        </div>
+        <div class="col-md-2 mb-3">
+             <label for="custom-select">份數 : ${edit_food_qty} </label>
+             <select class="custom-select" id="editfoodqty">
+                 <option selected>1</option>
+                 <option value="1">2</option>
+                 <option value="2">3</option>
+                 <option value="3">4</option>
+                 <option value="4">5</option>
+             </select>
+        </div>
+        <div class="col-md-3 mb-3">
+             <label for="exampleFormControlInput1">型態: ${edit_food_type}</label>
+             <select class="custom-select" id="editfoodtype">
+                 <option selected>早餐</option>
+                 <option value="1">午餐</option>
+                 <option value="2">晚餐</option>
+                 <option value="3">點心/其他</option>
+             </select>
+        </div>
+        <div class="col-md-1 mb-3">
+        <label for="exampleFormControlInput1">Save</label>
+        <button data-id="${edit_food_id}" class="button is-success is-small is-light js-save"><i class="fas fa-save"> </i></button>
+        </div>
+        </div>
+        `)       
+     })
+    }
     // Save edit daily record data function 
-    $('.daily_food_result').on('click','.js-save',function(){
-       alert(this.dataset.id);
+    $('.form-row').on('click','.js-save',function(evt){
+      // console.log(this.dataset.id)
+      let editfoodqty=$("#editfoodqty :selected").text();
+      let editfoodtype=$("#editfoodtype :selected").text();
+      let editfood_hash = {
+                            edit_id : `${this.dataset.id}`,
+                            edit_qty: editfoodqty,
+                            edit_type: editfoodtype}
+      // console.log(editfood_hash)
+      $(`.js-edit[data-id="${this.dataset.id}"]`).removeAttr('disabled')
+      $(this).parent().parent('.form-row').remove()
+      // $(`.js-edit`).filter(`[data-id="${this.dataset.id}"]`).removeAttr('disabled')
+
+      axios.patch(`http://localhost:3000/food_records/${this.dataset.id}`,editfood_hash)
+           .then( response => {
+                console.log('response=>',response);
+                $( "#daliy-food" ).load("search_food.html #daliy-food");
+                })
     })
-    // Save edit daily record data function 
-    $('.daily_food_result').on('click','.js-del',function(){
-       alert(this.dataset.id);
-    })
+
+    // Delete daily record data function
+    function deleteEvent(){    
+  
+      $('.daily_food_result').on('click','.js-del',function(evt){
+        console.log(this.dataset.id);
+        let delete_id = {id: this.dataset.id}
+        axios.delete(`http://localhost:3000/food_records/${this.dataset.id}`, delete_id )
+        .then( response => {
+            console.log('response=>',response);
+            $(this).parent().parent('.daily_food_result').remove()
+            })
+      })
+
+    }
+    deleteEvent()
+    editEvent()
+    //
     // Now time and day
     let time = moment().format('lll');
     $('.daytime').html(time);
