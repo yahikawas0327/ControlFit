2d796c4aeb5853faff772b6d0c0139ba57dd2fce
miluku1018 <miluku1018@gmail.com>
2020-01-05 17:49:51 +0800

    delete images

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 
 require("@rails/ujs").start()
@@ -23,3 +24,30 @@ import 'popper.js';
 import moment from "moment";
 import './sport_record.js'
 import 'bulma'
+=======
+
+
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+
+// require( "jquery3")
+// require( "popper")
+// require( "bootstrap-sprockets")
+
+
+
+import '@fortawesome/fontawesome-free/css/all.css'
+import '../stylesheets'
+import '../scripts'
+import $ from 'jquery'
+import axios from 'axios'
+// import "bootstrap-sprockets"
+import 'bootstrap';
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'popper.js';
+import moment from "moment";
+import './sport_record.js'
+import 'bulma'
+>>>>>>> delete images
