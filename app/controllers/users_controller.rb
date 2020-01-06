05d76f791abce8bcc2bf2abe1dad81ce6e9d0f6b
張維傑 <jie255057@gmail.com>
2020-01-06 14:05:19 +0800

    cover_image add variant

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_member!
   def index
-    @member = Member.all
+    @member = Member.all.with_attached_cover_image
   end
 
   def show
