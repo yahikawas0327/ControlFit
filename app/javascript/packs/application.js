a2525ac3820471b8ebc1add62fce59c92cc89700
miluku1018 <miluku1018@gmail.com>
2020-01-06 12:09:00 +0800

    solve conflict application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-
-
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-require.context('../images', true)
-// require( "jquery3")
-// require( "popper")
-// require( "bootstrap-sprockets")
-
-
-
-import '@fortawesome/fontawesome-free/css/all.css'
-import '../stylesheets'
-import '../scripts'
-import $ from 'jquery'
-import axios from 'axios'
-// import "bootstrap-sprockets"
-import 'bootstrap';
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import 'popper.js';
-import moment from "moment";
-import './sport_record.js'
-import 'bulma'
-=======
 
 
 require("@rails/ujs").start()
@@ -50,4 +23,3 @@ import 'popper.js';
 import moment from "moment";
 import './sport_record.js'
 import 'bulma'
->>>>>>> delete images
