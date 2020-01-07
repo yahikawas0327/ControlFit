624aeba572906be8d75f584e4206069ef7525bba
張維傑 <jie255057@gmail.com>
2020-01-07 19:28:24 +0800

    update follow js

--- app/javascript/packs/follow.js
+++ app/javascript/packs/follow.js
@@ -1,10 +1,2 @@
 import $ from 'jquery'
 import axios from 'axios'
-
-$(document).on('turbolinks:load',function(){
-  var myURL = new URL('http://localhost:5000/users/3');
-  console.log(myURL.pathname)
-  $('.btn-primary').click(function(even){
-    event.preventDefault();
-  })
-})
