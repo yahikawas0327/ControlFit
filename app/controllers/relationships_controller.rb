9d7441bc4650254d185badb1aa5c89b84549a097
張維傑 <jie255057@gmail.com>
2020-01-07 12:01:55 +0800

    update relationship controller

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -6,6 +6,7 @@ class RelationshipsController < ApplicationController
     current_member.follow(@member)
     respond_to do |format|
       format.html { redirect_to user_path(id: @member.id) }
+      format.js
     end
   end
 
@@ -14,6 +15,7 @@ class RelationshipsController < ApplicationController
     current_member.unfollow(@member)
     respond_to do |format|
       format.html { redirect_to user_path(id: @member.id) }
+      format.js
     end
   end
 end
