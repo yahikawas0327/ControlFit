aae556a937709d085d837a14c49762a4e90a6367
張維傑 <jie255057@gmail.com>
2020-01-13 15:08:43 +0800

    update bmi index

--- app/javascript/packs/bmi.js
+++ app/javascript/packs/bmi.js
@@ -17,7 +17,7 @@ $(document).on('turbolinks:load',function(){
       w = parseFloat(w);
       let bmi = w / (h * h);
       bmi = bmi.toFixed(2);
-      $(".box").html("\n BMI = " + bmi);
+      $(".box").html("BMI 稱為身體質量指數，是目前美國疾病管制局及世界衛生組織所認可，利用身高為基礎來測量體重是否符合標準，是目前國際上通用評估胖瘦的方式，理想體重範圍為 BMI 介於 18.5 到 24 之間，年輕者適用較低的 BMI 值，年長者則適用較高的 BMI 值，而最健康的 BMI 值為 22，若身體質量指數超過 24 為過重，超過 27 為肥胖，若超過 35 則為極度肥胖。 <br>" + "\n BMI = " + `<div class="d-inline-block bmi-jquery">${bmi}</div>` + '</br><br> 想更加認識自己嗎？ <a class="text-primary" href="http://localhost:5000/blogs">加入我們</a>')
     }
   })
 })
