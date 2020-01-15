e1e8949375a1dac91ca5413431dd6db6727c42b9
HenryYHHsu <henryhsu0529@gmail.com>
2020-01-15 19:19:09 +0800

    food-record system layout modify && RWD layout 0115

--- app/javascript/scripts/index.js
+++ app/javascript/scripts/index.js
@@ -10,6 +10,7 @@ import { element, func } from 'prop-types';
 import echarts from "echarts";
 
 // const axios = require('axios');
+// http://localhost:5000
  
 document.addEventListener('turbolinks:load', () => {
     // 增加 Query system event
@@ -39,6 +40,7 @@ document.addEventListener('turbolinks:load', () => {
     favorite()
     favoriteAdd()
     fooduserguide()
+    favoriteRwd()
     // Now time and day
     let time = moment().format('lll');
     $('.daytime').html(time);
@@ -102,7 +104,7 @@ function search(){
     $('#Add_food_record_type').hide()
     evt.stopPropagation();
     let searchfood_hash = {searchfood: $('#searchfood').val()}
-    axios.get('https://controlfit.online/search_food.json', {params:{ search_food: $('#searchfood').val()}})
+    axios.get('http://localhost:5000/search_food.json', {params:{ search_food: $('#searchfood').val()}})
          .then( response => {
           let query_data = response.data.length
           if (query_data !== 0){
@@ -276,7 +278,7 @@ function click_create_new_record_by_user(){
        }
        console.log(food_hash)
        $(this).parent().parent('.form-row').remove()
-   axios.post("https://controlfit.online/food_records", food_hash)
+   axios.post("http://localhost:5000/food_records", food_hash)
         .then( response => {
           console.log('response=>',response);
             let pre_time =  moment().format('LT'); 
@@ -391,7 +393,7 @@ function saveEvent(){
     $(`.js-edit[data-id="${this.dataset.id}"]`).removeAttr('disabled')
     $(this).parent().parent('.form-row').remove()
     let before_edit_sum = $(`.js-edit[data-id="${this.dataset.id}"]`).parent().siblings('.foodsum:eq(0)').text()
-    axios.patch(`https://controlfit.online/food_records/${this.dataset.id}`, editfood_hash)
+    axios.patch(`http://localhost:5000/food_records/${this.dataset.id}`, editfood_hash)
 
          .then( response => {
               // console.log('response=>',response);
@@ -419,7 +421,7 @@ function deleteEvent(){
   $('#daliy-food').on('click','.js-del',function(evt){
     console.log(this.dataset.id);
     let delete_id = {id: this.dataset.id}
-    axios.delete(`https://controlfit.online/food_records/${this.dataset.id}`, delete_id )
+    axios.delete(`http://localhost:5000/food_records/${this.dataset.id}`, delete_id )
     .then( response => {
         console.log('response=>',response);
         let del_sum = $(`.js-del[data-id="${this.dataset.id}"]`).parent().siblings('.foodsum:eq(0)').text()
@@ -450,7 +452,7 @@ function query_add(){
                }
     
      $(this).parent().parent('.form-row').remove()
-    axios.post("https://controlfit.online/food_records", food_hash)
+    axios.post("http://localhost:5000/food_records", food_hash)
          .then( response => {
                 console.log(response)
             let pre_time =  moment().format('LT'); 
@@ -486,6 +488,7 @@ function query_add(){
 function more(){
   $('.userfoodrecord').on('click','.js-more',function(evt){
     $('#daliy-food').toggle()
+    $('#fooduserstatus').toggle()
   }) 
 }
 
@@ -493,7 +496,7 @@ function more(){
 function recommend(){
   $('.userfoodrecord').on('click','.js-recommend',function(evt){
     $('#recommend-food').toggle()
-    axios.get('https://controlfit.online/search_food/random.json')
+    axios.get('http://localhost:5000/search_food/random.json')
          .then( response => {
                var result = ""
               for ( var i = 0; i < 5; i++) {
@@ -511,6 +514,7 @@ function recommend(){
                      </tr>
                           `}
               $('#recommend').html(result)
+              $('#fooduserstatus').toggle()
               })
     }) 
 }
@@ -618,8 +622,9 @@ function searchLike(){
 
 // favorite list 
 function favorite(){
-  $('#user_favorite_list').on('click',function(){ 
-    axios.get('https://controlfit.online/search_food/list')
+  $('#user_favorite_list').on('click',function(){
+ 
+    axios.get('http://localhost:5000/search_food/list')
          .then( response => {
            var result = ""
            for ( var i = 0; i < response.data.length; i++) {
