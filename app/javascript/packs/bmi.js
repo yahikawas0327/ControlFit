55a82e5e460176c979713a184f931b6740c9f4be
張維傑 <jie255057@gmail.com>
2020-01-16 12:45:52 +0800

    merge master

--- app/javascript/packs/bmi.js
+++ app/javascript/packs/bmi.js
@@ -16,18 +16,18 @@ $(document).on('turbolinks:load',function(){
       bmi = bmi.toFixed(2);
       let range = bmi
       if(range < 18.5){
-        range =  "體重輕到要飄起來了"
+        range =  "體重輕到要飄起來了  (•ิ_•ิ)?"
       }else if(range >= 18.5 && range < 24 ){
-        range = "體位正常"
+        range = "體位正常  ヾ(*´∇`)ﾉ"
       }else if(range >= 24 && range <= 27){
-        range = "有點胖"
+        range = "有點胖  Ψ(｀∀´#)ﾉ"
       }else if(range <= 30 && range <= 35){
-        range = "有點肥胖"
+        range = "有點肥胖  ( ´(00)`)"
       }else{
-        range = "重度肥胖了啦"
+        range = "重度肥胖了啦  (ఠ్ఠ ˓̭ ఠ్ఠ)"
       }
       console.log('123')
-      $(".box").html("\n BMI = " + `<div class="d-inline-block bmi-jquery">${bmi}</div>` + `<div>${range}</div>` + '<br> 想更加認識自己嗎？ <a class="text-primary" href="http://localhost:5000/blogs">加入我們</a>')
+      $(".box").html("\n BMI = " + `<div class="d-inline-block bmi-jquery">${bmi}</div>` + `<div class="bmi-range">${range}</div>` + '<br> 想更加認識自己嗎？ <a class="text-primary" href="http://localhost:5000/blogs">加入我們</a>')
     }
   })
 })
